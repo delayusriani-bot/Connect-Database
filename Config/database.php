dfe23c4a8344e063b044eaa589a6a629d5a4e5c5
DEBY RISYATI DATU <delayusriani@gmail.com>
2025-11-11 18:00:34 +0800

    Connect-Database

---
+++ Config/database.php
@@ -0,0 +1,17 @@
+<?php
+try {
+  // buat koneksi dengan database
+  $dbh = new PDO('mysql:host=localhost;dbname=universitas', "root", "");
+  
+  // set error mode
+  $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+  
+  // hapus koneksi
+  $dbh = null;
+}
+catch (PDOException $e) {
+  // tampilkan pesan kesalahan jika koneksi gagal
+  print "Koneksi atau query bermasalah: " . $e->getMessage() . "<br/>";
+  die();
+}
+?>
---
+++ Config/test_koneksi.php
@@ -0,0 +1,5 @@
+<?php
+require_once __DIR__ . '/database.php';
+echo "Koneksi ke database berhasil ✅";
+?>
+
